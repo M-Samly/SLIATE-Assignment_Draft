addc64646de42f1cf64c5d954f8b93af22fb3a4e
M-Samly <m.shamlymhmd@gmail.com>
2024-03-14 12:23:29 +0530

    created insert function and view function.

---
+++ src/code/studentCode.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package code;
+
+/**
+ *
+ * @author M.Samly
+ */
+public class studentCode {
+    int s_id;
+    String name;
+    String department;
+    int year;
+    String email;
+    String contact;
+
+    public studentCode(int s_id, String name, String department, int year, String email, String contact) {
+        this.s_id = s_id;
+        this.name = name;
+        this.department = department;
+        this.year = year;
+        this.email = email;
+        this.contact = contact;
+    }
+
+    public int getS_id() {
+        return s_id;
+    }
+
+    public void setS_id(int s_id) {
+        this.s_id = s_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+    
+    
+}
